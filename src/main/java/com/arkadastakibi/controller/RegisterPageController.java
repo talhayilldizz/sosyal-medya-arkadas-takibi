7e5f4fcbf0b88fbd4f34a368b60d1dd39d74fa15
Talha Yildiz <talhayilldizz@users.noreply.github.com>
2025-12-17 23:16:22 +0300

    Register sayfası dosyaya bağlanıp kullanıcı ekleme işlemi yapıldı. Logini yapacağım

--- src/main/java/com/arkadastakibi/controller/RegisterPageController.java
+++ src/main/java/com/arkadastakibi/controller/RegisterPageController.java
@@ -1,5 +1,6 @@
 package com.arkadastakibi.controller;
 
+import com.arkadastakibi.model.DataBase;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -7,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -15,24 +17,34 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class RegisterPageController implements Initializable {
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+
+
+
+
+public class RegisterPageController extends DataBase implements Initializable {
+    @FXML
+    private TextField txtFirstName;
+    @FXML
+    private TextField txtLastName;
     @FXML
     private TextField txtUsername;
-
     @FXML
     private TextField txtEmail;
-
     @FXML
     private PasswordField txtPassword;
-
     @FXML
     private PasswordField txtConfirmPassword;
-
     @FXML
     private Button btnRegister;
-
     @FXML
     private Hyperlink linkLogin;
+    @FXML
+    private Label lblMessage;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -42,27 +54,51 @@ public class RegisterPageController implements Initializable {
     }
 
     private void handleRegister(){
+        String firstName = txtFirstName.getText();
+        String lastName = txtLastName.getText();
         String username = txtUsername.getText();
         String email = txtEmail.getText();
         String password = txtPassword.getText();
         String confirmPassword = txtConfirmPassword.getText();
 
-        //Şimdilik konsola yazdır
-        System.out.println("Kayıt İşlemi Başlatıldı:");
-        System.out.println("Kullanıcı Adı: " + username);
-        System.out.println("Email: " + email);
-
-        if(username.isEmpty() || email.isEmpty() || password.isEmpty()) {
-            System.out.println("Hata: Lütfen tüm alanları doldurun!");
-            return; // Alanlar boşsa yönlendirme yapma
-        }
-
-        if(!password.equals(confirmPassword)) {
-            System.out.println("Şifreler Uyuşmuyor..");
+        if(firstName.isEmpty() || lastName.isEmpty() || username.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()){
+            lblMessage.setText("Boş Alanları Doldurunuz!");
+            lblMessage.setVisible(true);
             return;
         }
 
-        navigateToLogin();
+        if(!password.equals(confirmPassword)) {
+            lblMessage.setText("Şifreler Uyuşmuyor!");
+            lblMessage.setVisible(true);
+            return;
+        }
+
+
+        //Veriyi json dosyasına ekler
+        boolean success=saveUserToFile(firstName,lastName,username,email,password);
+
+        if(!success){
+            return;
+        }
+
+        handleRegister();
+    }
+
+    public JSONObject createUserDataJson(int id,String fName, String lName, String uName, String mail, String pass){
+        JSONObject data=new JSONObject();
+        data.put("id",id);
+        data.put("firstName",fName);
+        data.put("lastName",lName);
+        data.put("username",uName);
+        data.put("email",mail);
+        data.put("password",pass);
+
+        data.put("bio",JSONObject.NULL);
+        data.put("instagram",JSONObject.NULL);
+        data.put("twitter",JSONObject.NULL);
+        data.put("tiktok",JSONObject.NULL);
+
+        return data;
     }
 
     private void navigateToLogin(){
@@ -77,6 +113,79 @@ public class RegisterPageController implements Initializable {
         }catch (IOException e) {
             e.printStackTrace();
             System.out.println("Hata: Login sayfası yüklenemedi. Dosya yolunu kontrol et.");
+        }
+    }
+
+    //Kullanıcıya id üretir
+    private int getNextID(JSONArray usersArray){
+        int maxID=0;
+        for(int i=0;i<usersArray.length();i++){
+            JSONObject user=usersArray.getJSONObject(i);
+
+            if(user.has("id")){
+                int currentID=user.getInt("id");
+                if(currentID>maxID){
+                    maxID=currentID;
+                }
+            }
+        }
+
+        return maxID +1;
+    }
+
+    //Aynı mail ve kullanıcı adı kontrolü
+    private boolean isUserExist(JSONArray usersArray,String username,String email){
+        for(int i=0;i<usersArray.length();i++){
+            JSONObject user=usersArray.getJSONObject(i);
+
+            String tempUsername=user.getString("username");
+            String tempMail=user.getString("email");
+
+            if(tempUsername.equals(username) || tempMail.equals(email)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    //Kullanıcıyı dosyaya ekler
+    private boolean saveUserToFile(String fName,String lName,String uName,String mail,String pass){
+        try{
+           File file=new File("users.json");
+
+           JSONArray usersArray;
+
+           if(file.exists()){
+               String content = new String(Files.readAllBytes(file.toPath()));
+               usersArray = content.isEmpty() ? new JSONArray() : new JSONArray(content);
+           }
+           else{
+               usersArray = new JSONArray();
+           }
+
+           if(isUserExist(usersArray,uName,mail)){
+               lblMessage.setText("Bu Mail veya Kullanıcı Adı Sistemde Kayıtlı");
+               lblMessage.setVisible(true);
+               return false;
+           }
+
+           int newID=getNextID(usersArray);
+           JSONObject user=createUserDataJson(newID,fName,lName,uName,mail,pass);
+
+           usersArray.put(user);
+
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write(usersArray.toString(4));
+            }
+
+            return true;
+
+
+        }catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
---
+++ src/main/java/com/arkadastakibi/model/DataBase.java
@@ -0,0 +1,5 @@
+package com.arkadastakibi.model;
+
+public abstract class DataBase {
+
+}
